4ad82ecd46889a6c647303322615a5094ed5197f
yem1546 <87500366+yem1546@users.noreply.github.com>
2022-09-16 08:26:54 -0400

    Posts and search bar are showing

--- src/App.js
+++ src/App.js
@@ -5,17 +5,32 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, { useState } from 'react';
+import data from "./dummy-data"
+import Posts from "./components/Posts/Posts"
+import SearchBar from './components/SearchBar/SearchBar';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
 // Import the dummyData
 import './App.css';
 
 const App = () => {
+  const [posts, setPosts] = useState(data)
+  const [searchTerm, setSearchTerm] = useState("")
+  console.log(data)
+  console.log(posts)
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
 
   const likePost = postId => {
+    const updatedPosts = posts.map(post => {
+      if(post.id === postId){
+        return {...posts, likes: post.likes += 1}
+      } else {
+        return post
+      }
+      setPosts(updatedPosts)
+    })
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
 
@@ -32,6 +47,8 @@ const App = () => {
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
+      <SearchBar/>
+      <Posts likePost={likePost} posts={posts}/>
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -5,9 +5,13 @@ import './Posts.css';
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
-
+  
   return (
     <div className='posts-container-wrapper'>
+      
+      {props.posts.map((post, i) => {
+        return <Post post={post} key={i}/>
+      })}
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
